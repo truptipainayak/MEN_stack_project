5f6cd9d45f1a3c15d529558199fce7111f3c582a
TruptiPaiNayak <truptipai95@gmail.com>
2020-09-21 15:37:28 -0300

    initial commit

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 
 const port = process.env.PORT || 5000;
