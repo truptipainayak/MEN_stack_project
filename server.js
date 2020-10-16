fcb573e6a534ad11212c38d0ffaf34d64540443c
TruptiPaiNayak <truptipai95@gmail.com>
2020-10-16 15:24:49 -0300

    proxy

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ if (process.env.NODE_ENV === 'production') {
 });
 }
 
-const port = process.env.PORT;
 
-app.listen(port, () => console.log("Server started on port 5000....."));
+const PORT = process.env.PORT || 5000;
+console.log(PORT);
+app.listen(PORT);
