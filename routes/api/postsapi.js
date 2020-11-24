601c6570b777c1df6771e9de388a7174a37f30d7
TruptiPaiNayak <truptipai95@gmail.com>
2020-11-24 09:09:22 -0400

    changes

--- routes/api/postsapi.js
+++ routes/api/postsapi.js
@@ -26,6 +26,71 @@ router.post("/", (req, res) => {
   newPost.save().then(theResult => res.json(theResult));
 });
 
+//@route post /api/items
+//@desc update an item
+//@access public
+// router.post('/:id', (req, res) => {
+//   const updatePost = new Post({
+//     title: req.body.title,
+//     description: req.body.description
+//   });
+//   updatePost.save().then(theResult => res.json(theResult));
+// });
+
+// router.put('/:id', (req, res) => {
+//   Post.findbyId(req.params.id)
+//      .then(post => post.update(req.body));
+// })
+
+
+// router.route('/update/:id').post((req, res) => {
+//   Post.findById(req.params.id)
+//     .then(post => {
+//       post.title = req.body.title,
+//       post.description = req.body.description
+    
+//       post.save()
+//         .then(() => res.json('Todo updated!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+//     })
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+// newTodo.save()
+//   .then(() => res.json('Todo added!'))
+//   .catch(err => res.status(400).json('Error: ' + err));
+// });
+
+router.post("/update/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      post.title = req.body.title,
+      post.description = req.body.description
+
+      post.save()
+        .then(() => res.json('Todo updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+// router.put('/:id', (req, res) => {
+//   Post.findById(req.params.id, function(err, todo) {
+//       if (!post)
+//           res.status(404).send("data is not found");
+//       else
+//           title = req.body.title;
+//           description = req.body.description;
+//           post.save().then(todo => {
+//               res.json('Todo updated!');
+//           })
+//           .catch(err => {
+//               res.status(400).send("Update not possible");
+//           });
+//   });
+// });
+
 //@route delete /api/items/:id
 //@desc delete an item
 //@access public
